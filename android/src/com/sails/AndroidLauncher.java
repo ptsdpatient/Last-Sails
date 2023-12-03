a0ce87b279a2e143eecec3d6c59f14b98027fe95
Tanishq Dhote <tanishqbakka1@gmail.com>
2023-12-03 11:39:01 +0530

    Initial commit

---
+++ android/src/com/sails/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sails;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sails.SailsClass;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SailsClass(), config);
+	}
+}
